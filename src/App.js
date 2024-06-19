1ce21151171b29939fc2c4e7ad8ad917acfe00c0
felipeSilv4Dev <felipesilva.developer@gmail.com>
2024-06-19 19:45:28 -0300

    adding friends components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 const initialFriends = [
   {
     id: 118836,
@@ -22,7 +20,47 @@ const initialFriends = [
 ];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
 }
 
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You owe {friend.name} are even.</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
 export default App;
